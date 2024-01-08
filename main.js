7b2ac8b44c9db7a3483171371d44336a553b369f
Alexander Söderwall <alexander.soderwall@gmail.com>
2024-01-08 15:37:33 +0100

    Added security to calculate function

--- main.js
+++ main.js
@@ -4,23 +4,76 @@ const interestRateInput = document.getElementById("interestRate");
 const monthlyPaymentsInput = document.getElementById("monthlyPayments");
 const submitButton = document.getElementById("submitButton");
 submitButton.addEventListener("click", calculatePaymentPlan);
-// let loanAmount: number;
-// let interestRate: number;
-// let monthlyPayments: number;
-let m = 0;
+const errorMessage = document.getElementById("errorMessage");
+let monthlyCost = 0;
 let interestExpense = 0;
+const loanAmountUpperLimit = 10000000;
+const loanAmountLowerLimit = 10000;
+const interestRateUpperLimit = 30;
+const interestRateLowerLimit = 0.1;
+const monthlyPaymentsUpperLimit = 600;
+const monthlyPaymentsLowerLimit = 0;
 function calculatePaymentPlan() {
+    errorMessage.innerHTML = "";
+    interestRateInput.value = interestRateInput.value.replace(",", ".");
     let loanAmount = parseFloat(loanAmountInput.value);
     let interestRate = parseFloat(interestRateInput.value);
     let monthlyInterestRate = interestRate / 1200;
     let monthlyPayments = parseFloat(monthlyPaymentsInput.value);
-    m =
+    console.log(loanAmount);
+    if (Number.isNaN(loanAmount) === true) {
+        errorMessage.textContent = "Lånemängd måste vara ett nummer.";
+        return;
+    }
+    else if (Number.isNaN(interestRate) === true) {
+        errorMessage.textContent = "Räntesatsen måste vara ett nummer.";
+        return;
+    }
+    else if (Number.isNaN(monthlyPayments) === true) {
+        errorMessage.textContent = "Månadsbetalningar måste vara ett nummer.";
+        return;
+    }
+    if (loanAmountLowerLimit > loanAmount || loanAmountUpperLimit < loanAmount) {
+        if (loanAmountUpperLimit < loanAmount) {
+            errorMessage.textContent = "Lånemängden får max vara 10 miljoner.";
+            return;
+        }
+        else {
+            errorMessage.textContent = "Lånemängden får minst vara 10000";
+            return;
+        }
+    }
+    if (interestRateLowerLimit > interestRate ||
+        interestRateUpperLimit < interestRate) {
+        if (interestRateUpperLimit < interestRate) {
+            errorMessage.textContent = "Räntesatsen får max vara 30% miljoner.";
+            return;
+        }
+        else {
+            errorMessage.textContent = "Räntesatsen får minst vara 0.1%";
+            return;
+        }
+    }
+    if (monthlyPaymentsLowerLimit >= monthlyPayments ||
+        monthlyPaymentsUpperLimit < monthlyPayments) {
+        if (monthlyPaymentsUpperLimit < monthlyPayments) {
+            errorMessage.textContent =
+                "Låneperioden får max vara 600 månader (50 år).";
+            return;
+        }
+        else {
+            errorMessage.textContent = "Låneperioden måste vara över 0";
+            return;
+        }
+    }
+    monthlyCost =
         loanAmount *
             (monthlyInterestRate *
                 (Math.pow(1 + monthlyInterestRate, monthlyPayments) /
                     (Math.pow(1 + monthlyInterestRate, monthlyPayments) - 1)));
-    console.log(Math.ceil(m));
+    console.log(Math.ceil(monthlyCost));
     interestExpense = loanAmount * monthlyInterestRate * monthlyPayments;
+    //Orginal beloppet - interestExpense = räntekostnaden över hela låneperioden
     console.log(interestExpense);
     console.log(Math.pow(1 + monthlyInterestRate, monthlyPayments) - 1);
     console.log(Math.pow(1 + monthlyInterestRate, monthlyPayments));
--- main.ts
+++ main.ts
@@ -11,23 +11,82 @@ const submitButton = document.getElementById(
   "submitButton"
 ) as HTMLButtonElement;
 submitButton.addEventListener("click", calculatePaymentPlan);
-// let loanAmount: number;
-// let interestRate: number;
-// let monthlyPayments: number;
-let m: number = 0;
+const errorMessage = document.getElementById(
+  "errorMessage"
+) as HTMLParagraphElement;
+
+let monthlyCost: number = 0;
 let interestExpense: number = 0;
 
+const loanAmountUpperLimit: number = 10000000;
+const loanAmountLowerLimit: number = 10000;
+const interestRateUpperLimit: number = 30;
+const interestRateLowerLimit: number = 0.1;
+const monthlyPaymentsUpperLimit: number = 600;
+const monthlyPaymentsLowerLimit: number = 0;
+
 function calculatePaymentPlan(): void {
+  errorMessage.innerHTML = "";
+  interestRateInput.value = interestRateInput.value.replace(",", ".");
   let loanAmount: number = parseFloat(loanAmountInput.value);
   let interestRate: number = parseFloat(interestRateInput.value);
   let monthlyInterestRate: number = interestRate / 1200;
   let monthlyPayments: number = parseFloat(monthlyPaymentsInput.value);
-  m =
+  console.log(loanAmount);
+
+  if (Number.isNaN(loanAmount) === true) {
+    errorMessage.textContent = "Lånemängd måste vara ett nummer.";
+    return;
+  } else if (Number.isNaN(interestRate) === true) {
+    errorMessage.textContent = "Räntesatsen måste vara ett nummer.";
+    return;
+  } else if (Number.isNaN(monthlyPayments) === true) {
+    errorMessage.textContent = "Månadsbetalningar måste vara ett nummer.";
+    return;
+  }
+
+  if (loanAmountLowerLimit > loanAmount || loanAmountUpperLimit < loanAmount) {
+    if (loanAmountUpperLimit < loanAmount) {
+      errorMessage.textContent = "Lånemängden får max vara 10 miljoner.";
+      return;
+    } else {
+      errorMessage.textContent = "Lånemängden får minst vara 10000";
+      return;
+    }
+  }
+
+  if (
+    interestRateLowerLimit > interestRate ||
+    interestRateUpperLimit < interestRate
+  ) {
+    if (interestRateUpperLimit < interestRate) {
+      errorMessage.textContent = "Räntesatsen får max vara 30% miljoner.";
+      return;
+    } else {
+      errorMessage.textContent = "Räntesatsen får minst vara 0.1%";
+      return;
+    }
+  }
+
+  if (
+    monthlyPaymentsLowerLimit >= monthlyPayments ||
+    monthlyPaymentsUpperLimit < monthlyPayments
+  ) {
+    if (monthlyPaymentsUpperLimit < monthlyPayments) {
+      errorMessage.textContent =
+        "Låneperioden får max vara 600 månader (50 år).";
+      return;
+    } else {
+      errorMessage.textContent = "Låneperioden måste vara över 0";
+      return;
+    }
+  }
+  monthlyCost =
     loanAmount *
     (monthlyInterestRate *
       (Math.pow(1 + monthlyInterestRate, monthlyPayments) /
         (Math.pow(1 + monthlyInterestRate, monthlyPayments) - 1)));
-  console.log(Math.ceil(m));
+  console.log(Math.ceil(monthlyCost));
   interestExpense = loanAmount * monthlyInterestRate * monthlyPayments;
   //Orginal beloppet - interestExpense = räntekostnaden över hela låneperioden
   console.log(interestExpense);
